7e773792fcbcaf11615e0dcce4189e892779ac5e
autogestion <nozdre@gmail.com>
2018-06-20 22:48:22 +0300

    user info endpoint

--- pubgate/api/v1/user.py
+++ pubgate/api/v1/user.py
@@ -1,6 +1,7 @@
 
 from sanic import response, Blueprint
 from sanic_openapi import doc
+from little_boxes.key import Key
 
 from pubgate.db.models import User
 
@@ -26,6 +27,63 @@ async def create_user(request):
     return response.json({'zrada': 'nope'})
 
 
-@user_v1.route('/<user_id', methods=['GET'])
+@user_v1.route('/<user_id>', methods=['GET'])
+@doc.summary("Returns user details")
 async def get_user(request, user_id):
-    return response.json({'my': user_id})
+    user = await User.find_one(dict(username=user_id))
+    if not user:
+        return response.json({"error": "no such user"}, status=404)
+
+    domain = request.app.config.DOMAIN
+    key = Key(f"https://{domain}/user/{user_id}")
+    key.new()
+    return response.json(
+        {
+            "@context": [
+                "https://www.w3.org/ns/activitystreams",
+                "https://w3id.org/security/v1",
+                {
+                    "manuallyApprovesFollowers": "as:manuallyApprovesFollowers",
+                    "sensitive": "as:sensitive",
+                    "movedTo": "as:movedTo",
+                    "Hashtag": "as:Hashtag",
+                    "ostatus": "http://ostatus.org#",
+                    "atomUri": "ostatus:atomUri",
+                    "inReplyToAtomUri": "ostatus:inReplyToAtomUri",
+                    "conversation": "ostatus:conversation",
+                    "toot": "http://joinmastodon.org/ns#",
+                    "Emoji": "toot:Emoji",
+                    "focalPoint": {
+                        "@container": "@list",
+                        "@id": "toot:focalPoint"
+                    },
+                    "featured": "toot:featured",
+                    "schema": "http://schema.org#",
+                    "PropertyValue": "schema:PropertyValue",
+                    "value": "schema:value"
+                }
+            ],
+            "id": f"https://{domain}/user/{user_id}",
+            "type": "Person",
+            "following": f"https://{domain}/outbox/{user_id}/following",
+            "followers": f"https://{domain}/outbox/{user_id}/followers",
+            "inbox": f"https://{domain}/inbox/{user_id}/inbox",
+            "outbox": f"https://{domain}/outbox/{user_id}/outbox",
+            "preferredUsername": f"{user_id}",
+            "name": "",
+            "summary": "<p></p>",
+            "url": f"https://{domain}/@{user_id}",
+            "manuallyApprovesFollowers": False,
+            "publicKey": key.to_dict(),
+            "tag": [],
+            "attachment": [],
+            "endpoints": {
+                "sharedInbox": f"https://{domain}/inbox"
+            },
+            "icon": {
+                "type": "Image",
+                "mediaType": "image/png",
+                "url": "https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/512x512/torii.png"
+            }
+        }
+    )
