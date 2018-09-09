6e80b7a1090a7f1bdbac45bb1587948b42c26686
autogestion <nozdre@gmail.com>
2018-09-09 16:18:37 +0300

    return security

--- pubgate/api/v1/renders.py
+++ pubgate/api/v1/renders.py
@@ -6,6 +6,7 @@ from pubgate import LOGO
 
 context = [
         "https://www.w3.org/ns/activitystreams",
+        "https://w3id.org/security/v1",
         {
             "Hashtag": "as:Hashtag",
             "sensitive": "as:sensitive"
