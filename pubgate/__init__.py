fe2cc30dd335a03b73079c516f5f1768f8418dc8
Mykhailo Kushchenko <nozdre@gmail.com>
2019-04-29 22:04:52 +0300

    updates to support reverse  (#30)
    * updates to support reverse https://github.com/autogestion/pubgate-telegram/issues/1
    
    * revert queries
    
    * support media

--- pubgate/__init__.py
+++ pubgate/__init__.py
@@ -1,6 +1,9 @@
 import os
 
-__version__ = "0.2.9"
+__version__ = "0.2.10"
+
 
 KEY_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "storage")
+MEDIA = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "media")
 LOGO = "http://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/512x512/torii.png"
+
