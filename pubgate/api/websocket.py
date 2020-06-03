225e1fd85aa0994b52990a45dc392490b8f5fc81
mk <nozdre@gmail.com>
2020-06-03 01:56:04 +0100

    some cargo-programming

--- pubgate/api/websocket.py
+++ pubgate/api/websocket.py
@@ -1,11 +1,13 @@
 import asyncio
 
 import socketio
+from sanic.log import logger
 from sanic import Blueprint
 
 ws = Blueprint('ws_v1')
 
-sio = socketio.AsyncServer(async_mode='sanic')
+
+sio = socketio.AsyncServer(async_mode='sanic', cors_allowed_origins=[])
 
 class X:
     sockets = {}
@@ -14,16 +16,54 @@ class X:
 @sio.event
 async def connect(sid, environ):
     print('connect ', sid)
+    await sio.emit('my event', {'data': 'foobar'})
     print(environ)
-    z = environ['wsgi.input']
-    # zz = await z
-    print(z)
-    user_id = 'x'
-    sio.enter_room(sid, user_id)
+    await sio.emit('event', {'data': 'foobar2'}, room=sid)
+    # z = environ['wsgi.input']
+    # # zz = await z
+    # print(z)
+    # user_id = 'x'
+    # sio.enter_room(sid, user_id)
+
+    await sio.emit('my event', {'data': 'foobar'})
+
 
 @sio.event
 async def disconnect(sid):
     print('disconnect ', sid)
+
+# @sio.event
+# async def auth(sid, data):
+#     print("-auth---------")
+#     print(data)
+
+
+# @sio.on('auth')
+# def f1(sid, data):
+#     print('auth2-----------------')
+#     sio.emit('auth', 'auth', room=sid)
+#     sio.emit('auth', 'auth')
+#     sio.emit('ping1', 'pong', room=sid)
+#     sio.emit('ping1', 'pong')
+#     sio.emit('ping', 'pong1', room=sid)
+#     sio.emit('ping', 'pong1')
+
+@sio.on('ping')
+def ping(sid, data):
+    logger.warn('ping!')
+    return 'panda', 13
+
+@sio.on('panda')
+def panda(sid, data):
+    logger.warn('panda!')
+
+@sio.on('panda')
+def another_event(sid, data):
+    print('ping-----------------')
+    logger.debug('panda')
+    sio.emit('panda', data, room=sid)
+    sio.emit('panda', data)
+    sio.emit('panda', 'fish')
 
 #
 # @ws.websocket('/socket')
